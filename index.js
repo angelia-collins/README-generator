e020992f7aeecf04ed4e071bd5b6e07c78e2e9a8
angelia collins <angiecollins09@gmail.com>
2020-12-04 18:51:41 -0700

    run the questions

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-// const generateMarkdown = require('./utils/generateMarkdown');
-// const util = require('util');
 
 // const writeFileAsync = util.promisify(fs.writeFile);
 
@@ -56,15 +54,7 @@ inquirer.prompt([
   },
 ]);
 
-// function to write README file
-// function writeToFile(title, data) {
-//   fs.writeFile(title, data, err => {
-//     if (err) {
-//       return console.log(err);
-//     }
-//   });
-// }
-// function generateMarkdown(data) {
+
   var markdown = `# ${questions.title}
   Get a unique password based on your specifications.
   
@@ -74,11 +64,11 @@ inquirer.prompt([
   ## Check It Out
   [Generate a Password](https://angelia-collins.github.io/PasswordGenerator/)
   
- ©2020"
+ ©2020
 
 `;
-// }
 
+questions();
 
 fs.writeFile('README.md', markdown, (err) => {
   if (err) {
@@ -86,33 +76,4 @@ fs.writeFile('README.md', markdown, (err) => {
         }
         console.log('readme had been made.')
 });
-//   `"# Password Generator
-//   Get a unique password based on your specifications.
-  
-//   ## What It Looks Like
-//   ![password generator](/Assets/password-generator-screencap.png)
-  
-//   ## Check It Out
-//   [Generate a Password](https://angelia-collins.github.io/PasswordGenerator/)
-  
-//  ©2020"`
-  
 
-
-// // function to initialize program
-// function init() {
-
-// }
-
-// // function call to initialize program
-// init();
-
-// questions()
-// .then(generateMarkdown())
-
-// .then((title, data) => writeFileAsync('README.md', questions(title, data)))
-// .then(writeToFile());
-// .then(generateMarkdown());
-  // .then((title, data) => writeFileAsync('README.md', questions(title, data)))
-  // .then(() => console.log('Successfully wrote to index.html'))
-  // .catch((err) => console.error(err));;
