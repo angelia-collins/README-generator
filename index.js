2f0291411e57d3c07bcb51aeb8c4f46d35fc3472
angelia collins <angiecollins09@gmail.com>
2020-12-04 17:13:33 -0700

    adding the markdown template

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const util = require('util');
+// const util = require('util');
+// const generateMarkdown = require('./utils/generateMarkdown');
 
-const writeFileAsync = util.promisify(fs.writeFile);
+// const writeFileAsync = util.promisify(fs.writeFile);
 
-// array of questions for user
+// prompts
 const questions = () =>
 inquirer.prompt([
   {
     type: 'input',
-    name: 'name',
+    name: 'title',
     message: 'Project name?',
   },
   {
@@ -56,8 +57,18 @@ inquirer.prompt([
 ]);
 
 // function to write README file
-function writeToFile(fileName, data) {
-    //put the readme file bs here
+function writeToFile(title, data) {
+  `"# Password Generator
+  Get a unique password based on your specifications.
+  
+  ## What It Looks Like
+  ![password generator](/Assets/password-generator-screencap.png)
+  
+  ## Check It Out
+  [Generate a Password](https://angelia-collins.github.io/PasswordGenerator/)
+  
+ ©2020"`
+  
 }
 
 // // function to initialize program
@@ -69,3 +80,7 @@ function writeToFile(fileName, data) {
 // init();
 
 questions();
+writeToFile()
+  // .then((title, data) => writeFileAsync('README.md', questions(title, data)))
+  // .then(() => console.log('Successfully wrote to index.html'))
+  // .catch((err) => console.error(err));;
